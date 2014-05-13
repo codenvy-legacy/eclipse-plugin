ea5de23ee7e11d3353d64cc6f934c93cf21f45f5
Kevin Pollet <pollet.kevin@gmail.com>
2014-05-13 16:45:47 +0200

    #33 Add update handler.

---
+++ com.codenvy.eclipse.ui/src/main/java/com/codenvy/eclipse/ui/team/UpdateHandler.java
@@ -0,0 +1,33 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * ________________
+ * 
+ * [2012] - [2014] Codenvy, S.A.
+ * All Rights Reserved.
+ * NOTICE: All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any. The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.eclipse.ui.team;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * Handler updating resource from Codenvy.
+ * 
+ * @author Kevin Pollet
+ */
+public class UpdateHandler extends AbstractHandler {
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        return null;
+    }
+}
