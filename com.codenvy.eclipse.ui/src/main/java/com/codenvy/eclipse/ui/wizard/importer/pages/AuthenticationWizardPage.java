90b5ab6f6876d48cfcb7bc51cb8010e9a225228b
Stéphane Daviet <stephane.daviet@serli.com>
2014-05-22 14:05:31 +0200

    Close #44 on username selection bug
    The username field was not filled with when an username was selected
    in the dropdown list.

--- com.codenvy.eclipse.ui/src/main/java/com/codenvy/eclipse/ui/wizard/importer/pages/AuthenticationWizardPage.java
+++ com.codenvy.eclipse.ui/src/main/java/com/codenvy/eclipse/ui/wizard/importer/pages/AuthenticationWizardPage.java
@@ -238,6 +238,7 @@ public class AuthenticationWizardPage extends WizardPage implements IPageChangin
                 }
                 usernames.add(username);
             }
+            usernames.setText(currentUsername);
         }
     }
 
