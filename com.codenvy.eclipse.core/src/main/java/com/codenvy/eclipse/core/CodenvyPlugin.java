f40d7de6e8567cf4d6e28b284a863ef5dbb6371b
Kevin Pollet <pollet.kevin@gmail.com>
2014-05-22 09:59:03 +0200

    #12 Add an activator for the core plugin

---
+++ com.codenvy.eclipse.core/src/main/java/com/codenvy/eclipse/core/CodenvyPlugin.java
@@ -0,0 +1,52 @@
+/*
+ * CODENVY CONFIDENTIAL
+ * ________________
+ * 
+ * [2012] - [2014] Codenvy, S.A.
+ * All Rights Reserved.
+ * NOTICE: All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any. The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
+package com.codenvy.eclipse.core;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author Kevin Pollet
+ */
+public class CodenvyPlugin extends Plugin {
+    // the plug-in ID
+    public static final String   PLUGIN_ID = "com.codenvy.eclipse.core"; //$NON-NLS-1$
+
+    // the shared instance
+    private static CodenvyPlugin plugin;
+
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance.
+     * 
+     * @return the shared instance
+     */
+    public static CodenvyPlugin getDefault() {
+        return plugin;
+    }
+}
