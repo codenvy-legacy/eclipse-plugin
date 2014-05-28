213cb1598697ddde7feb51dd6c163917d09c0f0f
Kevin Pollet <pollet.kevin@gmail.com>
2014-05-28 11:57:27 +0200

    #46 Validate data in run main configuration tab

--- com.codenvy.eclipse.ui/src/main/java/com/codenvy/eclipse/ui/launcher/RunMainConfigurationTab.java
+++ com.codenvy.eclipse.ui/src/main/java/com/codenvy/eclipse/ui/launcher/RunMainConfigurationTab.java
@@ -18,10 +18,12 @@ package com.codenvy.eclipse.ui.launcher;
 
 
 import static com.codenvy.eclipse.core.launcher.LaunchConstants.CODENVY_PROJECT_NAME_ATTRIBUTE_NAME;
+import static com.codenvy.eclipse.core.utils.StringHelper.isEmpty;
 import static com.codenvy.eclipse.ui.Images.RUN_MAIN_TAB_ICON;
 import static org.eclipse.ui.model.WorkbenchLabelProvider.getDecoratingWorkbenchLabelProvider;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -29,6 +31,8 @@ import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
@@ -38,8 +42,10 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.team.core.RepositoryProvider;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 
+import com.codenvy.eclipse.core.team.CodenvyProvider;
 import com.codenvy.eclipse.ui.CodenvyUIPlugin;
 
 /**
@@ -50,7 +56,12 @@ import com.codenvy.eclipse.ui.CodenvyUIPlugin;
 public class RunMainConfigurationTab extends AbstractLaunchConfigurationTab {
     private static final String TAB_NAME = "Main";
 
-    private Text                projectLocation;
+    private Text                projectName;
+    private IWorkspaceRoot      workspaceRoot;
+
+    public RunMainConfigurationTab() {
+        this.workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+    }
 
     @Override
     public void createControl(Composite parent) {
@@ -62,15 +73,23 @@ public class RunMainConfigurationTab extends AbstractLaunchConfigurationTab {
         projectGroup.setLayout(new GridLayout(2, false));
         projectGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 
-        projectLocation = new Text(projectGroup, SWT.BORDER);
-        projectLocation.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+        projectName = new Text(projectGroup, SWT.BORDER);
+        projectName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+        projectName.addModifyListener(new ModifyListener() {
+            @Override
+            public void modifyText(ModifyEvent e) {
+                updateLaunchConfigurationDialog();
+            }
+        });
 
         final Button browse = createPushButton(projectGroup, "Browse...", null);
         browse.setFocus();
         browse.addSelectionListener(new SelectionListener() {
             @Override
             public void widgetSelected(SelectionEvent e) {
-                final ElementListSelectionDialog selectionDialog = new ElementListSelectionDialog(getShell(), getDecoratingWorkbenchLabelProvider());
+                final ElementListSelectionDialog selectionDialog =
+                                                                   new ElementListSelectionDialog(getShell(),
+                                                                                                  getDecoratingWorkbenchLabelProvider());
                 selectionDialog.setTitle("Project selection");
                 selectionDialog.setMessage("Select a project to constrain your search.");
                 selectionDialog.setMultipleSelection(false);
@@ -80,7 +99,7 @@ public class RunMainConfigurationTab extends AbstractLaunchConfigurationTab {
                 final Object[] resource = selectionDialog.getResult();
                 if (resource != null && resource.length > 0) {
                     final IProject project = (IProject)resource[0];
-                    projectLocation.setText(project.getName());
+                    projectName.setText(project.getName());
                 }
             }
 
@@ -94,6 +113,42 @@ public class RunMainConfigurationTab extends AbstractLaunchConfigurationTab {
     }
 
     @Override
+    public boolean isValid(ILaunchConfiguration configuration) {
+        final String projectName;
+        try {
+
+            projectName = configuration.getAttribute(CODENVY_PROJECT_NAME_ATTRIBUTE_NAME, "");
+
+        } catch (CoreException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (!isEmpty(projectName)) {
+
+            final IProject project = workspaceRoot.getProject(projectName);
+            final boolean isCodenvyProject =
+                                             project.exists()
+                                                 && RepositoryProvider.getProvider(project, CodenvyProvider.PROVIDER_ID) != null;
+            setErrorMessage(isCodenvyProject ? null : "Project specified is not a valid Codenvy project");
+
+            return isCodenvyProject;
+        }
+
+        setErrorMessage("Project not specified");
+        return false;
+    }
+
+    @Override
+    public boolean canSave() {
+        final String projectName = this.projectName.getText();
+        if (!isEmpty(projectName)) {
+            final IProject project = workspaceRoot.getProject(projectName);
+            return project.exists() && RepositoryProvider.getProvider(project, CodenvyProvider.PROVIDER_ID) != null;
+        }
+        return false;
+    }
+
+    @Override
     public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
         // no default configuration
     }
@@ -102,7 +157,7 @@ public class RunMainConfigurationTab extends AbstractLaunchConfigurationTab {
     public void initializeFrom(ILaunchConfiguration configuration) {
         try {
 
-            projectLocation.setText(configuration.getAttribute(CODENVY_PROJECT_NAME_ATTRIBUTE_NAME, ""));
+            projectName.setText(configuration.getAttribute(CODENVY_PROJECT_NAME_ATTRIBUTE_NAME, ""));
 
         } catch (CoreException e) {
             throw new RuntimeException(e);
@@ -111,14 +166,14 @@ public class RunMainConfigurationTab extends AbstractLaunchConfigurationTab {
 
     @Override
     public void performApply(ILaunchConfigurationWorkingCopy configuration) {
-        configuration.setAttribute(CODENVY_PROJECT_NAME_ATTRIBUTE_NAME, projectLocation.getText());
+        configuration.setAttribute(CODENVY_PROJECT_NAME_ATTRIBUTE_NAME, projectName.getText());
     }
 
     @Override
     public String getName() {
         return TAB_NAME;
     }
-    
+
     @Override
     public Image getImage() {
         final ImageRegistry imageRegistry = CodenvyUIPlugin.getDefault().getImageRegistry();
