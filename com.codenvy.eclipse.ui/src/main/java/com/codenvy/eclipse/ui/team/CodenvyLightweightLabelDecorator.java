c83d5cad7fa441b3f99eeefb7f3b1b973958c4f1
Kevin Pollet <pollet.kevin@gmail.com>
2014-06-30 14:56:48 +0200

    Close #97 Add a decorator to Codenvy projects

--- com.codenvy.eclipse.ui/src/main/java/com/codenvy/eclipse/ui/team/CodenvyLightweightLabelDecorator.java
+++ com.codenvy.eclipse.ui/src/main/java/com/codenvy/eclipse/ui/team/CodenvyLightweightLabelDecorator.java
@@ -16,15 +16,17 @@
  */
 package com.codenvy.eclipse.ui.team;
 
+import static org.eclipse.core.resources.IResource.PROJECT;
 import static org.eclipse.core.resources.IResource.ROOT;
 import static org.eclipse.team.core.RepositoryProvider.getProvider;
+import static org.eclipse.team.ui.ISharedImages.IMG_CHECKEDIN_OVR;
+import static org.eclipse.ui.ide.ResourceUtil.getAdapter;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IDecoration;
-import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ILightweightLabelDecorator;
-import org.eclipse.team.ui.ISharedImages;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.team.ui.TeamImages;
 import org.eclipse.ui.ide.ResourceUtil;
 
@@ -32,46 +34,34 @@ import com.codenvy.eclipse.core.team.CodenvyMetaResource;
 import com.codenvy.eclipse.core.team.CodenvyProvider;
 
 /**
- * The Codenvy label decorator.
+ * {@link ILightweightLabelDecorator} implementation used to decorate {@link IResource} of a project linked to Codenvy.
  * 
  * @author Kevin Pollet
+ * @see CodenvyProvider
  */
-public class CodenvyLightweightLabelDecorator implements ILightweightLabelDecorator {
+public class CodenvyLightweightLabelDecorator extends LabelProvider implements ILightweightLabelDecorator {
     private final ImageDescriptor trackedImageDescriptor;
 
     public CodenvyLightweightLabelDecorator() {
-        trackedImageDescriptor = TeamImages.getImageDescriptor(ISharedImages.IMG_CHECKEDIN_OVR);
-    }
-
-    @Override
-    public void addListener(ILabelProviderListener listener) {
-
-    }
-
-    @Override
-    public void dispose() {
-
-    }
-
-    @Override
-    public boolean isLabelProperty(Object element, String property) {
-        return false;
-    }
-
-    @Override
-    public void removeListener(ILabelProviderListener listener) {
-
+        trackedImageDescriptor = TeamImages.getImageDescriptor(IMG_CHECKEDIN_OVR);
     }
 
     @Override
     public void decorate(Object element, IDecoration decoration) {
         final IResource resource = ResourceUtil.getResource(element);
+
         if (resource != null && resource.getType() != ROOT) {
             final CodenvyProvider provider = (CodenvyProvider)getProvider(resource.getProject(), CodenvyProvider.PROVIDER_ID);
+
             if (provider != null) {
-                final CodenvyMetaResource metaResource = (CodenvyMetaResource)ResourceUtil.getAdapter(resource, CodenvyMetaResource.class, true);
+                final CodenvyMetaResource metaResource = (CodenvyMetaResource)getAdapter(resource, CodenvyMetaResource.class, true);
+
                 if (metaResource != null && metaResource.isTracked()) {
                     decoration.addOverlay(trackedImageDescriptor);
+
+                    if (resource.getType() == PROJECT) {
+                        decoration.addSuffix(" [codenvy: " + provider.getMetaProject().url + "]");
+                    }
                 }
             }
         }
